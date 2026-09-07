747af290457d2fddf97cae16c483ada59ac20cf9
hatemFakhrany <108612951+hatemFakhrany@users.noreply.github.com>
2026-09-07 11:51:37 +0300

    Add files via upload

---
+++ commands.js
@@ -0,0 +1,7 @@
+Office.onReady(() => {
+  // This file is loaded by commands.html.
+  // Your manifest currently uses ShowTaskpane actions, which don't need
+  // custom JS functions here. If you later add ExecuteFunction buttons,
+  // register their handlers with Office.actions.associate("functionName", handlerFn)
+  // and Office.actions.associate again for each additional button.
+});
